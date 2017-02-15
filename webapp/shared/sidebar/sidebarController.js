0d0030f433ee27395e4afda927fd6a3918fd2388
Joaquin Heredia <joaquinheredia93@gmail.com>
2017-02-15 02:37:45 -0300

    CSS modifications to sidebar

--- webapp/shared/sidebar/sidebarController.js
+++ webapp/shared/sidebar/sidebarController.js
@@ -3,19 +3,25 @@ var app = angular.module('Appelu');
 app.controller('SidebarCtrl', ['$scope','$timeout','$mdSidenav', function ($scope, $timeout, $mdSidenav) {
 	$scope.menuItems = [
 		{
-			name: "Perfil",
-			url: "/profile",
-			icon: "account_circle"
+			name: "Turnos",
+			url: "/shifts",
+			icon: "book"
 		},
 		{
 			name: "Reservas",
 			url: "/reserves",
 			icon: "assignment"
-		},
-		{
-			name: "Turnos",
-			url: "/shifts",
-			icon: "book"
+		},{
+			name: "Perfil",
+			url: "/profile",
+			icon: "account_circle"
 		}
+		
 	]
+
+	$scope.activeMenu = $scope.menuItems[0];
+
+	$scope.setActive = function(menuItem){
+		$scope.activeMenu = menuItem;
+	}
 }])
