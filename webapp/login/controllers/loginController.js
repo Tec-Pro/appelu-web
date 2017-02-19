047b98597653d074126fab70e42e0112394657b5
Alan Gonzalez <alan.gonzalez@mercadolibre.com>
2017-02-19 17:20:43 -0300

    login in progress

--- webapp/login/controllers/loginController.js
+++ webapp/login/controllers/loginController.js
@@ -3,6 +3,9 @@ var app = angular.module('Appelu');
 app.controller('LoginCtrl', ['$scope', '$location', function($scope,$location) {
 
     $scope.ingresar = function() {
+
+    	alert($scope.username)
+    	alert($scope.password)
         
         $location.path('/shifts');
 
