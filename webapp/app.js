a3f62bf8e7c83c251f5e3c7b2716158f9d3752b1
Joaquin Heredia <joaquinheredia93@gmail.com>
2017-02-20 17:29:06 -0300

    Hide sidebar on login

--- webapp/app.js
+++ webapp/app.js
@@ -7,7 +7,10 @@ var app = angular.module('Appelu', ['ngRoute', 'ngMaterial']);
 
 app.config(['$routeProvider',function($routeProvider) {
 	$routeProvider
-
+	.when("/profile",{
+		templateUrl: "webapp/components/profile/views/profile.html",
+		controller: "ProfileCtrl"
+	})
 	.when("/shifts", {
 		templateUrl: "webapp/components/shifts/views/list.html",
 		controller: "ShiftCtrl"
@@ -16,4 +19,7 @@ app.config(['$routeProvider',function($routeProvider) {
 		templateUrl: "webapp/components/login/views/login.html",
 		controller: "LoginCtrl"
 	})
+	.otherwise({
+		redirectTo: "/login"
+	});
 }])
---
+++ webapp/components/profile/controllers/profileController.js
@@ -0,0 +1,5 @@
+var app = angular.module('Appelu');
+
+app.controller('ProfileCtrl', ['$scope', function ($scope) {
+	
+}])
