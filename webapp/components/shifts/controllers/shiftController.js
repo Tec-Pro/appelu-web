9e6e906836dda292c4f3447a2e545fa1c4568895
Joaquin Heredia <joaquinheredia93@gmail.com>
2017-03-16 00:52:22 -0300

    Added shift resource

--- webapp/components/shifts/controllers/shiftController.js
+++ webapp/components/shifts/controllers/shiftController.js
@@ -1,28 +1,17 @@
 var app = angular.module('Appelu');
 
-app.controller('ShiftCtrl', ['$scope','$timeout','$mdSidenav', function ($scope, $timeout, $mdSidenav) {
+app.controller('ShiftCtrl', ['$scope','$timeout','$mdSidenav','shiftFactory', function ($scope, $timeout, $mdSidenav, shiftFactory) {
 	$scope.services = ["Peluquería", "Barbería"];
 
-    $scope.availableShifts = [
-    {
-    	id: 1665,
-      user_id: null,
-      service_id: 61,
-      comment: "reservita",
-      created_at: "2017-02-26T21:35:23.985Z",
-      updated_at: "2017-02-26T21:35:23.985Z",
-      start_time: "2017-03-01T12:00:00.000Z",
-      end_time: "2017-03-01T12:30:00.000Z",
-      status: "active"
-    },{
-			id: 16615,
-      user_id: null,
-      service_id: 61,
-      comment: "reservita",
-      created_at: "2017-02-26T21:35:23.985Z",
-      updated_at: "2017-02-26T21:35:23.985Z",
-      start_time: "2017-03-01T12:00:00.000Z",
-      end_time: "2017-03-01T12:30:00.000Z",
-      status: "disabled"
-    }];
+  function getShifts(){
+    shiftFactory.getServiceShifts(62).then(function(response){
+      $scope.shifts = response.data;
+      console.log(response)
+    }, function(error){
+      console.log(error);
+    })
+  }
+
+  getShifts();
+
 }])
---
+++ webapp/shared/services/shiftService.js
@@ -0,0 +1,35 @@
+var app = angular.module('Appelu');
+
+app.factory('shiftFactory', ['$http', function($http){
+
+	var urlBase = 'https://private-9195cfb-reservationsapp.apiary-mock.com/shifts'
+	var shiftFactory = {};
+
+	shiftFactory.getShift = function(id){
+		return $http.get(urlBase+'/'+id);
+	}
+
+	/** 
+	shiftFactory.createShift = function(shift){
+		return $http.post(urlBase);
+	}
+	*/
+
+	shiftFactory.deleteShift = function(id){
+		return $http.delete(urlBase+'/'+id);
+	}	
+
+	shiftFactory.updateShift = function(shift){
+		return $http.put(urlBase+'/'+shift.id, shift);
+	}
+
+	shiftFactory.getServiceShifts = function(serviceId){
+		return $http.get(urlBase+'?service_id='+serviceId);
+	}
+
+	shiftFactory.getUserShifts = function(userId){
+		return $http.get(urlBase+'?user_id='+userId);
+	}
+
+	return shiftFactory;
+}])
