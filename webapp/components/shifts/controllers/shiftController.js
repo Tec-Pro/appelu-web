3ea2139d72bf1b0850fcd0dafeadeb324b4b9161
Joaquin Heredia <joaquinheredia93@gmail.com>
2017-03-19 19:22:05 -0300

    added disable and enable shift

--- webapp/components/shifts/controllers/shiftController.js
+++ webapp/components/shifts/controllers/shiftController.js
@@ -14,4 +14,26 @@ app.controller('ShiftCtrl', ['$scope','$timeout','$mdSidenav','shiftFactory', fu
 
   getShifts();
 
+  $scope.deleteShift = function(id, index){
+    console.log("Hello "+id+" "+index);
+    shiftFactory.deleteShift(id).then(function(response){
+      $scope.shifts[index].attributes.status = "disabled";      
+      console.log($scope.shifts);
+    }, function(error){
+      console.log(error);
+    });
+  }
+
+  $scope.enableShift = function(shift){
+    var updatedShift = shift;
+    updatedShift.attributes.status = "active";
+    shiftFactory.updateShift(updatedShift).then(function(response){
+      shift.attributes.status = "active";
+      console.log($scope.shifts);
+    }, function(error){
+      console.log(error);
+    })
+  }
+
+  
 }])
