98abb8cf83ab3f5d97c9dcf62620c775bc6ed9fc
Joaquin Heredia <joaquinheredia93@gmail.com>
2017-04-21 02:59:11 -0300

    Removed profile from app, added reserves

--- webapp/app.js
+++ webapp/app.js
@@ -7,9 +7,9 @@ var app = angular.module('Appelu', ['ngRoute', 'ngMaterial', 'md.data.table']);
 
 app.config(['$routeProvider',function($routeProvider) {
 	$routeProvider
-	.when("/profile",{
-		templateUrl: "webapp/components/profile/views/profile.html",
-		controller: "ProfileCtrl"
+	.when("/reserves",{
+		templateUrl: "webapp/components/reserves/views/list.html",
+		controller: "ReserveCtrl"
 	})
 	.when("/shifts", {
 		templateUrl: "webapp/components/shifts/views/list.html",
